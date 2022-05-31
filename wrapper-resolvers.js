ed9c8846093106d2faf9dd9be2fd5b791ad1b71b
Pontus Jarnemyr <pontusjarnemyr@gmail.com>
2022-05-31 11:15:54 +0200

    Wrapping paths now have a viable solution

--- wrapper-resolvers.js
+++ wrapper-resolvers.js
@@ -1,4 +1,4 @@
-const { wrapSchema, introspectSchema, RenameTypes, WrapFields, WrapQuery, MapFields, MapLeafValues, RenameObjectFields } = require('@graphql-tools/wrap');
+const { wrapSchema, WrapQuery, introspectSchema, RenameTypes, WrapFields, MapFields, MapLeafValues, RenameObjectFields } = require('@graphql-tools/wrap');
 const { fetch } = require("cross-fetch");
 const { delegateToSchema } = require("@graphql-tools/delegate");
 const { print } = require("graphql/language");
@@ -21,11 +21,32 @@ const remoteSchema = async () => {
     return wrapSchema({
     schema,
     executor,
-    /*transforms: [
-        new RenameObjectFields((_typeName, fieldName) => fieldName.replace(/^title/, "emailAddress"))
-    ]*/
+    transforms: [
+        new RenameObjectFields((_typeName, fieldName) => fieldName.replace(/^title/, "myTitle"))
+    ]
     });
 };
+
+const createField = (name) => {
+    return {
+        kind: Kind.FIELD,
+        name: {
+            kind: Kind.NAME,
+            value: name,
+        },
+        arguments: [],
+        directives: [],
+    }
+};
+
+const createTwoLayeredField = (name, fields) => {
+    const field = createField(name);
+    field.selectionSet = {
+        kind: Kind.SELECTION_SET,
+        selections: fields
+    }
+    return field;
+}
 
 const resolvers = {
     Query: {
@@ -53,36 +74,55 @@ const resolvers = {
                 info,
                 transforms: [
                     new WrapQuery(
-                    // path at which to apply wrapping and extracting
-                        ['tracksForHome'], 
+                        ["tracksForHome"],
                         (subtree) => {
-                        var newSelectionSet = {
-                            kind: Kind.SELECTION_SET,
-                            selections: subtree.selections.map(function (selection) {
-                                // just append fragments, not interesting for this test
-                                if (selection.kind === Kind.INLINE_FRAGMENT || selection.kind === Kind.FRAGMENT_SPREAD) {
-                                    return selection;
-                                }
-                                // prepend `address` to name and camelCase
-                                var oldFieldName = selection.name.value;
-                                return {
-                                    kind: Kind.FIELD,
-                                    name: {
-                                        kind: Kind.NAME,
-                                        value: oldFieldName,
-                                    },
-                                };
-                            }),
-                        };
-                        console.log(newSelectionSet.selections);
-                        return newSelectionSet;
-                    },
-                    result => { 
-                        console.log(result);
-                        return result; 
-                    }), 
+                            //console.log(subtree);
+                            const newSelectionSet = {
+                                kind: Kind.SELECTION_SET,
+                                selections: subtree.selections.map(selection => {
+                                    if(selection.name.value === "authorPhoto") {
+                                        return {
+                                            kind: Kind.FIELD,
+                                            name: {
+                                                kind: Kind.NAME,
+                                                value: "author"
+                                            },
+                                            selectionSet: {
+                                                kind: Kind.SELECTION_SET,
+                                                selections: [{
+                                                    kind: Kind.FIELD,
+                                                    name: {
+                                                        kind: Kind.NAME,
+                                                        value: "photo"
+                                                    }   
+                                                }]
+                                            }
+                                        }
+                                    } else {
+                                        return selection;
+                                    }
+                                }),
+                              };
+                              //console.log(newSelectionSet.selections[2]);
+                              return newSelectionSet;
+                        },
+                        (result) => {
+                            //authorPhoto: result.author.photo
+                            result.forEach(function(element) {
+                                element.authorPhoto = element.author.photo;
+                            })
+                            console.log(result);
+                            return result;
+                        }
+                    ),
+                    new WrapQuery(
+                        ['tracksByHome', 'authorPhoto'],
+                        (subtree) => subtree,
+                        result => result
+                    ),
                 ]
-            })
+            });
+            //console.log(data);
             return data;
         },
         myModule: async(_, args, context, info) => {
@@ -99,6 +139,27 @@ const resolvers = {
             })
             return data;
         },
+        /*myAuthor: async(_, args, context, info) => {
+            const schema = await remoteSchema();
+            const result = await execute({
+                schema,
+                document: parse(`
+                  query {
+                    Author(id: "cat-1") {
+                        id
+                        name
+                    }
+                  }
+                `),
+              });
+            
+            return result.data.tracksForHome.forEach(function(result) {
+                if(result.author.id === args.id){
+                    console.log(result.author);
+                    return result.author;
+                }
+            });
+        }*/
     }
 }
 module.exports = resolvers;    
